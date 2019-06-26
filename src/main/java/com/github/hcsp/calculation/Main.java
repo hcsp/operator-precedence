a325c51dc306d9098ac155964f5e6dddd67890a5
东方来客 <39690418+westee@users.noreply.github.com>
2019-06-26 23:06:38 +0800

    优先级 (#15)
    优先级 (#15)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp.calculation;
 
 public class Main {
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        return (a == null ? "" : a ) + (b == null ? "" : b);
     }
 
     public static void main(String[] args) {
