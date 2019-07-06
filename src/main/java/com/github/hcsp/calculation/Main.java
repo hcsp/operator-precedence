7a7c6cc1e52ff9ef5be7b3ffa68faf569625694d
苏赵勇 <sutaojie@gmail.com>
2019-07-06 14:27:06 +0800

    处理空指针异常

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,8 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        String str = a != null ? (b != null ? a + b : a) : (b != null ? b : "");
+        return str;
     }
 
     public static void main(String[] args) {
