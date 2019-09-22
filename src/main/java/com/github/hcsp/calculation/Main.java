cb5baa66e7d78a20f76f5fc79f4101d240359d4f
svlm <lmyykyo@163.com>
2019-09-22 21:40:01 +0800

    - [x] (#55)
    - [x] (#55)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,19 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+//        if(a == null){
+//            return b == null ? "" : b;
+//        }else {
+//            return b == null ? a : a + b;
+//        }
+
+        String ans = "";
+        if(a != null)
+            ans += a;
+        if(b != null){
+            ans += b;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
@@ -12,5 +24,6 @@ public class Main {
         System.out.println(concatString("a", null));
         System.out.println(concatString("a", "b"));
         System.out.println(concatString(null, "b"));
+
     }
 }
