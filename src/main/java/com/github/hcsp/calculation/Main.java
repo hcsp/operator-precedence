409434a0c20246dd6127a7ec4e55922f02a4f1ff
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2019-11-27 21:05:38 +0800

    修复优先级问题。 (#88)
    修复优先级问题。 (#88)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,22 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+//        if (a != null) {
+//            if (b != null) {
+//                //a!=null&&b!=null
+//                return a + b;
+//            }else {
+//                //b==null
+//                return a;
+//            }
+//        } else {
+//            if (b != null) {
+//                //a==null
+//                return b;
+//            }
+//        }
+//        return "";
+        return (a != null) ? ((b != null) ? a + b : a) : (b != null ? b : "");
     }
 
     public static void main(String[] args) {
