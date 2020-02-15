97baee6a2bb0a7e4cac397974daaa32ccbeb226f
SuperPounch <1240847431@qq.com>
2020-02-15 20:06:30 +0800

    a+b

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,9 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        String A = (a == null) ? "" : a;
+        String B = (b == null) ? "" : b;
+        return A + B;
     }
 
     public static void main(String[] args) {
