171f03ddd631e767b8b08fbd7d14a2098dce2fdd
7952126 <15521608275@163.com>
2020-03-23 18:42:46 +0800

    ace

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,7 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "": a + b == null ? "" :b==null?"":"a+b";
+        return (a == null ? "": a )+ (b == null ? "" :b);
         //a是空值嘛，如果是，输出“”，
         // 如果a不是空值，那a+b是空值嘛，如果是，输出“”，
         // 如果a+b不是空值，应该输出的是a+b，但实际上只是输出了b
