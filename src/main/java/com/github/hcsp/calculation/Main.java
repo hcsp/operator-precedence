d4e7e1597ebf9bced6c0862a5625c77125df2fef
DWB-Frank <57584830+DWB-Frank@users.noreply.github.com>
2020-05-04 01:05:30 +0900

    my commit; (#176)
    my commit; (#176)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,9 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        String temp1 = a == null ? "" : a;
+        String temp2 = b == null ? "" : b;
+        return temp1 + temp2;
     }
 
     public static void main(String[] args) {
