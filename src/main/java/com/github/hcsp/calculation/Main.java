46f12dc27f159f3068bc349fa46d97e49faedeee
Haixiang <haixiang6123@gmail.com>
2020-05-28 16:38:44 +0800

    解决优先级不当引发的bug (#188)
    解决优先级不当引发的bug (#188)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,10 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        String c = a == null ? "" : a;
+        String d = b == null ? "" : b;
+
+        return c + d;
     }
 
     public static void main(String[] args) {
