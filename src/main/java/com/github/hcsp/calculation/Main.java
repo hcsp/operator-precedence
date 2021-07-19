19aeb2befd5d2b6859f2c96845557f2ebb1ae0d9
lynnyanggod <ouyangyly@gmail.com>
2021-07-19 17:00:26 +0800

    fixed the null bug

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,14 +1,14 @@
 package com.github.hcsp.calculation;
 
-public class  Main {
+public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
         String temp;
-        if( a == null){
+        if (a == null) {
             a = "";
         }
-        if(b == null ){
+        if (b == null) {
             b = "";
         }
         return a + b;
