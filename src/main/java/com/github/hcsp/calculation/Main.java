61532b69abb7254948fd77b2d01a34ffae394fab
thomaszzzzz <84179177+thomaszzzzz@users.noreply.github.com>
2021-10-10 00:08:31 +0800

    null和"" (#243)
    null和"" (#243)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,7 +4,16 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        if(a==null)
+            if(b==null)
+                return "";
+            else
+                return b;
+        else
+            if(b==null)
+                return a;
+            else
+                return a+b;
     }
 
     public static void main(String[] args) {
