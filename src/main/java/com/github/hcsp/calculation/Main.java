903f4a97d5ffddd1d7a7d18f643dff22b5fde8ad
lx <iamxliu@163.com>
2022-03-29 20:27:26 +0800

    2

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -3,7 +3,6 @@ package com.github.hcsp.calculation;
 public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
-
     public static String concatString(String a, String b) {
         return (a == null ? "" : a) + (b == null ? "" : b);
     }
