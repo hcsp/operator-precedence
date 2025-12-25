c323812d108e6efd843f38c735e6306cebee02ae
LatteCosmos <loading5083@outlook.com>
2025-12-26 10:44:13 +1100

    要捋清楚 && ｜｜以及三元符号的逻辑

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,13 +4,13 @@ public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        return a == null && b==null ? "" :(a==null && b!=null) ?b : (a!=null && b==null) ? a:(a!=null && b!=null )?a+b:"";
     }
 
     public static void main(String[] args) {
-        System.out.println(concatString(null, null));
-        System.out.println(concatString("a", null));
-        System.out.println(concatString("a", "b"));
-        System.out.println(concatString(null, "b"));
+        System.out.println(concatString(null, null)); //""
+        System.out.println(concatString("a", null));   //"a"
+        System.out.println(concatString("a", "b"));    // a+b
+        System.out.println(concatString(null, "b"));   // b
     }
 }
